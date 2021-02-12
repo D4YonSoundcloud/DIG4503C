3f1599bb121700fbc3e04f5c7f2b907ed43e145a
d4yonsouncloude <mattd4y@gmail.com>
2021-02-12 14:25:06 -0500

    added lab 4 2:24pm 2-12-21

--- lab3/lab3/src/components/Homepage.js
+++ lab3/lab3/src/components/Homepage.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 class HomePage extends React.Component {
-
     render() {
-
         return <div>
             <h1> Hello, { this.props.firstName }</h1>
             { this.props.showQuestion === true && <h2>do you prefer react, angular, or vue?</h2> }
