082b47bfa5f916a19607d1780ec4b50ae451e98d
d4yonsouncloude <mattd4y@gmail.com>
2021-02-12 14:26:55 -0500

    lmao forgot to push all of it 2:25pm 2-12-21

---
+++ lab4/lab4/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Market from './components/Market'
+import './App.css'
+
+
+function App() {
+  return (
+    <Market class="inventory"/>
+  );
+}
+
+export default App;
