86fdf212c0ed07524105b097ca2534e9a28f1935
d4yonsouncloude <mattd4y@gmail.com>
2021-04-30 12:26:09 -0400

    finished?

--- lab11-portfolio2/index.js
+++ lab11-portfolio2/index.js
@@ -1,47 +1,41 @@
 import MongoClient from 'mongodb'
 import Express from 'express';
 const App = Express();
+//Cors is needed so we can actually get request from our Express Server
 import cors from 'cors'
 const port = 3333;
 
+//url for connecting to our database
+
+//name of the collection
+let collectionName = "Tricks-Final-Final"
+
+//create variable to store our tricks
 let tricks = []
-MongoClient.connect(URL, { useUnifiedTopology: true })
-	.then(connection => {
-		let database = connection.db('Kendama-Tricks');
-
-		let collection = database.collection("Tricks-Test")
-
-		let cursor = collection.find({level: 1})
-
-		tricks = []
-		cursor.forEach(document => {
-			console.log(document.name);
-		}, () => {
-			connection.close();
-		})
-	})
-	.catch(error => {
-		console.log("Error: " + error);
-	})
 
 //Enables Cross-Origin Resource Sharing, so that we can actually get request
 App.use(cors());
 
 /**
- * GET level
+ * GET level + grip
  */
 App.get('/level/:level/grip/:grip', (req, res) => {
+
 	let result = {"error": "no tricks of this level found"};
-	console.log(parseFloat(req.params.level))
+	//get level + grip from the params
 	let levelToFind = parseFloat(req.params.level);
 	let gripToFind = req.params.grip
+
+	//connect to our Database
 	MongoClient.connect(URL, { useUnifiedTopology: true })
 		.then(connection => {
 			let database = connection.db('Kendama-Tricks');
-			let collection = database.collection("Tricks-Test")
+			let collection = database.collection(collectionName)
+			//Query for tricks that match our criteria
 			let cursor = collection.find({ level: levelToFind, grip: gripToFind })
 			tricks = []
 			cursor.forEach(document => {
+				//push a new object for each trick that has its name, level, and grip
 				tricks.push(
 					{
 						name: document.name,
@@ -49,13 +43,12 @@ App.get('/level/:level/grip/:grip', (req, res) => {
 						grip: document.grip,
 					}
 				);
-				console.log(tricks);
-				console.log(document.name);
 			}, () => {
-				console.log(result);
+				//set the result to the tricks array of trick objects
 				result = tricks;
-				console.log('it has been fulfilled', result)
+				//close the database connection
 				connection.close();
+				//return the tricks array so our client can use it
 				return res.json(result);
 			})
 		})
@@ -64,13 +57,19 @@ App.get('/level/:level/grip/:grip', (req, res) => {
 		})
 })
 
+/**
+ * Get Level, no grip
+ */
 App.get('/level/:level', (req, res) => {
+
 	let result = {"error": "no tricks of this level found"};
+	//get level from the params
 	let levelToFind = parseFloat(req.params.level);
 	MongoClient.connect(URL, { useUnifiedTopology: true })
 		.then(connection => {
+			//see above comments
 			let database = connection.db('Kendama-Tricks');
-			let collection = database.collection("Tricks-Test")
+			let collection = database.collection(collectionName)
 			let cursor = collection.find({ level: levelToFind })
 			tricks = []
 			cursor.forEach(document => {
